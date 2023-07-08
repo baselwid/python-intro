6575fbd71886f481d3f0f4ce4f371ad186c3962c
Belal Edries <108571254+Edries-Belal@users.noreply.github.com>
2023-07-08 11:58:36 +0300

    learn about  index and slice for string

---
+++ main.py
@@ -0,0 +1,21 @@
+# var1 ='value'
+# print (type(var1))
+# print (var1[0])
+# print (var1[-1])
+# print (var1[1:3])
+# print(len(var1))
+# var2= 5
+# print (type(var2))
+
+# var3 = 3.14
+# print (type(var3))
+
+# var4 = True
+# print(type(var4))
+
+# var5 = 5+6j
+# print (type(var5))
+str1 ='hello'
+str1= 'y'+str1[1:len(str1)]
+print (str1)
+print(str1[0:2]+'mm'+str1[4:len(str1)])
