9f3066d9ac2d4cfb9877c921eff6141cea2be74a
baselwid <baselwid@gmail.com>
2023-08-03 15:53:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,31 @@
 
 # var5 = 5+6j
 # print (type(var5))
-str1 ='hello'
-str1= 'y'+str1[1:len(str1)]
-print (str1)
-print(str1[0:2]+'mm'+str1[4:len(str1)])
+# str1 ='hello'
+# str1= 'y'+str1[1:len(str1)]
+# print (str1)
+# print(str1[0:2]+'mm'+str1[4:len(str1)])
+
+student={"name":"","age":0,"country":'' ,"city":'',"job":'', 'skills': [], 'perant':{'father':'', 'mother':''}}
+print(student)
+
+for i in student:
+
+   if type(student[i])== int:
+
+          student[1] = int(input('enter the '+i+' :')) 
+     
+   elif type(student [i])== list:
+
+       times = int(input('how many skills do you have?'))
+       for i in range(times):
+            student [1].append(input('enter the '+i+' :'))
+
+   elif type(student [i])== dict:
+      student[i]['father']= input('enter the father name:')
+      student [i]['mother']= input('enter the mother name:')
+
+   else:
+        student [1]= input('enter the' + i+':')
+
+print(student)
