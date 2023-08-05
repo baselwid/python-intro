c0dcc1383d39eb6e5afe635f69e409530018d0da
baselwid <baselwid@gmail.com>
2023-08-05 09:24:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,26 +20,77 @@
 # print (str1)
 # print(str1[0:2]+'mm'+str1[4:len(str1)])
 
-student={"name":"","age":0,"country":'' ,"city":'',"job":'', 'skills': [], 'perant':{'father':'', 'mother':''}}
-print(student)
 
-for i in student:
+# student={"name":"","age":0,"country":'' ,"city":'',"job":'', 'skills': [], 'perant':{'father':'', 'mother':''}}
+# print(student)
 
-   if type(student[i])== int:
+# for i in student:
 
-          student[1] = int(input('enter the '+i+' :')) 
+#    if type(student[i])== int:
+
+#           student[1] = int(input('enter the '+i+' :')) 
      
-   elif type(student [i])== list:
+#    elif type(student [i])== list:
 
-       times = int(input('how many skills do you have?'))
-       for i in range(times):
-            student [1].append(input('enter the '+i+' :'))
+#        times = int(input('how many skills do you have?'))
+#        for i in range(times):
+#             student [1].append(input('enter the '+i+' :'))
 
-   elif type(student [i])== dict:
-      student[i]['father']= input('enter the father name:')
-      student [i]['mother']= input('enter the mother name:')
+#    elif type(student [i])== dict:
+#       student[i]['father']= input('enter the father name:')
+#       student [i]['mother']= input('enter the mother name:')
 
-   else:
-        student [1]= input('enter the' + i+':')
+#    else:
+#         student [1]= input('enter the' + i+':')
 
-print(student)
+# print(student)
+
+
+
+# students=[]
+# student={}
+# renter=True
+# while renter:
+#     insertTime=int(input("how many key do you want to add  "))
+#     for i in range(insertTime):
+#         deckey=input("enter the key")
+#         decvalueTybe=int(input("enter the tybe of the value where 1 for str 2 for int 3 for list 4 for dec"))
+#         if decvalueTybe==1:
+#             decvalueTybe=input("enter the value")
+#         elif decvalueTybe==2:
+#             decvalueTybe=int(input("enter the value"))
+#         elif decvalueTybe==3:
+#             decvalueTybe=[]
+#             item=int(input('hwo many '+deckey+'do you have?'))
+#             for j in range(item):
+#                 decvalueTybe.append(input('enter the '+ deckey +':'))
+#         elif decvalueTybe==4:
+#             decvalueTybe={}
+#             item=int(input('hwo many '+deckey+'do you have?')) 
+#             for j in range(item):
+#                 student[input('enter the key')]=input('enter the value') 
+
+#     student[deckey]=decvalue
+#     students.append(student)
+    
+# print(student)
+
+
+# class claint:
+#     name='basel'
+#     age=27
+#     city='irbid'
+# claint1=claint()
+# print(claint.name)
+# print(claint.age)   
+# claint.name="hashem"
+# print(claint.name)
+
+# class has attribute and method
+# what is the defference between attributes and method
+# attributes are varibale
+# method are function
+# car 
+# name:bmw
+# color:black
+#  
